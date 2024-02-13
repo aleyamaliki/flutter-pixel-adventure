de2d7e3ca62651df1ac3e31a6bb74b3517358e9e
AqilJaafree <wanaqilre@gmail.com>
2024-02-13 15:30:32 +0800

    fullscreen n landscape mode added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flame/game.dart';
+import 'package:flame/flame.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:pixel_adventure/pixel_adventure.dart';
 
 void main() {
+  
+  WidgetsFlutterBinding.ensureInitialized();
+  //fullscreen  mode on mobile devices
+  Flame.device.fullScreen();
+  //set on landscape
+  Flame.device.setLandscape();
   PixelAdventure game = PixelAdventure();
-  runApp(GameWidget(game: game));
+  //runApp(GameWidget(game: game));
+  //kdebug mode for testing
+  runApp(GameWidget(game: kDebugMode ? PixelAdventure() : game));
 }
 
