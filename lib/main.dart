cd8d4dd3b22447b872e8229443f2fe65cdc3ec5a
AqilJaafree <wanaqilre@gmail.com>
2024-02-13 15:09:16 +0800

    pixeladventure added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:pixel_adventure/pixel_adventure.dart';
 
 void main() {
+  PixelAdventure game = PixelAdventure();
   runApp(GameWidget(game: game));
 }
 
