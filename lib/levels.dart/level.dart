58629e756bb93801afe1adbfe995fed09928d26a
AqilJaafree <wanaqilre@gmail.com>
2024-02-14 22:32:45 +0800

    level added

---
+++ lib/levels.dart/level.dart
@@ -0,0 +1,13 @@
+import 'dart:async';
+import 'package:flame_tiled/flame_tiled.dart';
+import 'package:flame/components.dart';
+class Level extends World {
+
+
+
+  @override
+  FutureOr<void> onLoad() {
+    // TODO: implement onLoad
+    return super.onLoad();
+  }
+}
