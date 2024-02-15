4fa8cb0525bea06f9ca52917a7cd5985cfb8edb8
AqilJaafree <wanaqilre@gmail.com>
2024-02-15 22:51:14 +0800

    completed basic level 1

--- lib/levels.dart/level.dart
+++ lib/levels.dart/level.dart
@@ -3,11 +3,14 @@ import 'package:flame_tiled/flame_tiled.dart';
 import 'package:flame/components.dart';
 class Level extends World {
 
-
+  late TiledComponent level ;
 
   @override
-  FutureOr<void> onLoad() {
-    // TODO: implement onLoad
+  FutureOr<void> onLoad() async {
+    level  = await TiledComponent.load('Level-01.tmx',Vector2.all(16));
+
+    add(level);
+
     return super.onLoad();
   }
 }
