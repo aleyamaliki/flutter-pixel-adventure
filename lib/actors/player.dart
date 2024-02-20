2f9a6d76b58b91ebfd5826e4ac33d5c2376e37a0
AqilJaafree <wanaqilre@gmail.com>
2024-02-21 00:01:08 +0800

    running animation added

--- lib/actors/player.dart
+++ lib/actors/player.dart
@@ -8,6 +8,7 @@ enum PlayerState { idle, running }
 class Player extends SpriteAnimationGroupComponent 
   with HasGameRef<PixelAdventure> {
   late final SpriteAnimation idleAnimation;
+  late final SpriteAnimation runningAnimation;
   final double stepTime = 0.05;
 
 
@@ -26,11 +27,24 @@ class Player extends SpriteAnimationGroupComponent
          textureSize: Vector2.all(32),
         ),
       );
+
+    runningAnimation = SpriteAnimation.fromFrameData(
+    game.images.fromCache('Main Characters/Ninja Frog/Run (32x32).png'), 
+    SpriteAnimationData.sequenced(
+         amount: 12, 
+         stepTime: stepTime,
+         textureSize: Vector2.all(32),
+        ),
+      );
    
     // //List of all animations  
-     animations = { PlayerState.idle: idleAnimation } ;
+     animations = { 
+      PlayerState.idle: idleAnimation,
+      PlayerState.running: runningAnimation,
+     
+     } ;
      //set of current animation
-    current = PlayerState.idle;
+    current = PlayerState.running;
   }
 }
 
