55e4befe399e99fa6d3ba66eb3b40f359b69d8b2
AqilJaafree <wanaqilre@gmail.com>
2024-02-20 23:26:36 +0800

    bug player fixed

--- lib/actors/player.dart
+++ lib/actors/player.dart
@@ -5,33 +5,32 @@ import 'package:pixel_adventure/pixel_adventure.dart';
 
 enum PlayerState { idle, running }
 
-class Player extends SpriteAnimationComponent with HasGameRef<PixelAdventure> {
+class Player extends SpriteAnimationGroupComponent 
+  with HasGameRef<PixelAdventure> {
   late final SpriteAnimation idleAnimation;
   final double stepTime = 0.05;
 
-  // Define animations and current variables
-  late Map<PlayerState, SpriteAnimation> animations;
-  late PlayerState current;
 
   @override
   FutureOr<void> onLoad() {
     _loadAllAnimations();
     return super.onLoad();
   }
-
-  void _loadAllAnimations() {
+  
+  void _loadAllAnimations() { 
     idleAnimation = SpriteAnimation.fromFrameData(
-      game.images.fromCache('Main Characters/Ninja Frog/Idle (32x32).png'),
-      SpriteAnimationData.sequenced(
-        amount: 11,
-        stepTime: stepTime,
-        textureSize: Vector2.all(32),
-      ),
-    );
-
-    // List of all animations
-    animations = {PlayerState.idle: idleAnimation};
-    // Set the current animation
+    game.images.fromCache('Main Characters/Ninja Frog/Idle (32x32).png'), 
+    SpriteAnimationData.sequenced(
+         amount: 11, 
+         stepTime: stepTime,
+         textureSize: Vector2.all(32),
+        ),
+      );
+   
+    // //List of all animations  
+     animations = { PlayerState.idle: idleAnimation } ;
+     //set of current animation
     current = PlayerState.idle;
   }
 }
+
