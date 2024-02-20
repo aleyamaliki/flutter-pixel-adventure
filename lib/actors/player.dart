af4a04729bada3022b72a6dc26c33c0cb3987972
AqilJaafree <wanaqilre@gmail.com>
2024-02-20 23:09:21 +0800

    frog added

--- lib/actors/player.dart
+++ lib/actors/player.dart
@@ -3,24 +3,35 @@ import 'dart:async';
 import 'package:flame/components.dart';
 import 'package:pixel_adventure/pixel_adventure.dart';
 
+enum PlayerState { idle, running }
+
 class Player extends SpriteAnimationComponent with HasGameRef<PixelAdventure> {
   late final SpriteAnimation idleAnimation;
   final double stepTime = 0.05;
 
+  // Define animations and current variables
+  late Map<PlayerState, SpriteAnimation> animations;
+  late PlayerState current;
 
   @override
   FutureOr<void> onLoad() {
     _loadAllAnimations();
     return super.onLoad();
   }
-  
-  void _loadAllAnimations() { 
-    idleAnimation = SpriteAnimation.fromFrameData(game.images.fromCache('Main Characters/Ninja Frog/Idle (32x32).png'), SpriteAnimationData.sequenced(
-         amount: 11, 
-         stepTime: stepTime,
-         textureSize: Vector2.all(32),
-        ),
-      );
+
+  void _loadAllAnimations() {
+    idleAnimation = SpriteAnimation.fromFrameData(
+      game.images.fromCache('Main Characters/Ninja Frog/Idle (32x32).png'),
+      SpriteAnimationData.sequenced(
+        amount: 11,
+        stepTime: stepTime,
+        textureSize: Vector2.all(32),
+      ),
+    );
+
+    // List of all animations
+    animations = {PlayerState.idle: idleAnimation};
+    // Set the current animation
+    current = PlayerState.idle;
   }
 }
-
--- lib/levels/level.dart
+++ lib/levels/level.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flame_tiled/flame_tiled.dart';
 import 'package:flame/components.dart';
+import 'package:pixel_adventure/actors/player.dart';
 class Level extends World {
 
   late TiledComponent level ;
@@ -10,7 +11,7 @@ class Level extends World {
     level  = await TiledComponent.load('Level-01.tmx',Vector2.all(16));
 
     add(level);
-
+    add(Player());
     return super.onLoad();
   }
 }
